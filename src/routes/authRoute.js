ed09ee395053b50a06999969c1410f041463d3da
Rushikesh Nanotkar <rushikesh.nanotkar@neosoftmail.com>
2023-05-23 10:33:09 +0530

    update

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import { userLogin, userRegistration } from '../controllers/authController.js';
-
 import { authRules } from '../validations/authValidation.js';
 import { upload } from '../utils/multer.js';
 export default [
 
     // Public Routes
     router.post('/register', upload.single('profileImage'), userRegistration),
+    router.post('/login',userLogin ),
     // router.post('/login', [authRules.login, validationCheck, userLogin]),
 ]
